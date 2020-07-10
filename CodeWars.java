501185ace044429c2e5c639b1cb588b1141ce844
nbence94 <nbence094@gmail.com>
2020-07-10 20:46:39 +0200

    Plusz egy kis tanulás

--- CodeWars.java
+++ CodeWars.java
@@ -2,14 +2,47 @@ package abstractteszt;
 
 import java.util.Arrays;
 import java.util.stream.IntStream;
-import javax.xml.soap.Node;
 
 public class CodeWars {
        
     public static void main(String[] args) {
         System.out.println("oddOrEven: " + oddOrEven(new int[] {2, 5, 34, 6}));
-         
+        System.out.println("scramble: " + scramble("rkqodlw","world"));
+        System.out.println("longestConsec: " + longestConsec(new String[] {"zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"}, 2));       
     }  
+    
+    public static String longestConsec(String[] strarr, int k) {
+        if(k > strarr.length) return "";
+        String result = "";
+        String tmp = strarr[0];
+        for(int i = 0; i < k; i++){
+            for(int x = 0; x < strarr.length; x++){
+                if(strarr[x].length() > tmp.length() && !result.contains(strarr[x])){
+                    tmp = strarr[x];
+                } 
+            }
+            result += tmp;
+            tmp = strarr[0];
+        }           
+        return result;        
+    }    
+    
+    
+    
+    
+    public static boolean scramble(String str1, String str2) {    
+        int count = 0;
+        for(int i = 0; i < str2.length(); i++){
+            if(str1.contains(String.valueOf(str2.charAt(i)))) {
+                count++;
+                str1 = str1.replaceFirst(String.valueOf(str2.charAt(i)),"");
+            }
+        }
+        return (count == str2.length());
+    }    
+    
+     
+    
     
     public static String oddOrEven (int[] array)    {
         IntStream list = Arrays.stream(array);       
