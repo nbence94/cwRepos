074f3abe9df109150123fbe4ac1bf43bf8bb2fd5
nbence94 <n.bence094@gmail.com>
2020-08-18 21:13:13 +0200

    Frissítések

--- CodeWars.java
+++ CodeWars.java
@@ -1,8 +1,11 @@
 package onlineexces;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CodeWars {
@@ -14,26 +17,293 @@ public class CodeWars {
         System.out.println("toCamelCase: " + toCamelCase("the-stealth-warrior"));
         System.out.println("pigIt: " + pigIt2("Hello world !"));
         System.out.println("validate: " + validate("1230"));
+
+        //System.out.println(parseInt("three thousand forty-six"));
+        //System.out.println(parseInt_2("three thousand and three"));
+        
+        System.out.println("Eredmény: " + greedyRefactor(new int[] {1,1,1,3,1}));//Várt 1100
+        
+                int[][] sudoku = {
+                {5, 3, 4, 6, 7, 8, 9, 1, 2},
+                {6, 7, 2, 1, 9, 5, 3, 4, 8},
+                {1, 9, 8, 3, 4, 2, 5, 6, 7},
+                {8, 5, 9, 7, 6, 1, 4, 2, 3},
+                {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                {9, 6, 1, 5, 3, 7, 2, 8, 4},
+                {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                {3, 4, 5, 2, 8, 6, 1, 7, 9}
+                    /*{5, 3, 4, 6, 7, 8, 9, 1, 2}, 
+                    {6, 7, 2, 1, 9, 0, 3, 4, 8},
+                    {1, 0, 0, 3, 4, 2, 5, 6, 0},
+                    {8, 5, 9, 7, 6, 1, 0, 2, 0},
+                    {4, 2, 6, 8, 5, 3, 7, 9, 1},
+                    {7, 1, 3, 9, 2, 4, 8, 5, 6},
+                    {9, 0, 1, 5, 3, 7, 2, 1, 4},
+                    {2, 8, 7, 4, 1, 9, 6, 3, 5},
+                    {3, 0, 0, 4, 8, 1, 1, 7, 9}*/
+        };
+        System.out.println("Sudoku: " + check(sudoku));
+        System.out.println("Faktoriális: " + zeros(18));
         
     }  
-        public static String firstEnd(String str){
-        String mid = str.substring(1, str.length()-1);
-        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    
+    
+    
+    
+    
+    public static int zeros(int n) {
+      if(n == 0) return 0;
+      //long factorial = 1;
+      java.math.BigInteger factorial = BigInteger.ONE;
+      for(int i = n; i > 0; i--){
+          factorial = factorial.multiply(new BigInteger(i + ""));
+      }
+      String tmp = String.valueOf(factorial);
+      int result  = 0;
+      for(char a : tmp.toCharArray()){
+          if(a == '0') result++;
+      }
+      return result;
+    }   
+    
+    
+    public static boolean check2(int[][] sudoku) {
+        Map<Integer, Integer> sNums = new HashMap<>();
+        for(int i = 0; i < sudoku.length; i++){
+            for(int j = 0; j < sudoku.length; j++){
+                if(sNums.containsKey(sudoku[i][j])){
+                    sNums.put(sudoku[i][j], sNums.get(sudoku[i][j])+1);
+                } else sNums.put(sudoku[i][j], 1);
+            }
+        }    
+        return (sNums.get(1) == 9 && sNums.get(2) == 9 && sNums.get(3) == 9 && sNums.get(4) == 9 && sNums.get(5) == 9 && sNums.get(6) == 9 && sNums.get(7) == 9 && sNums.get(8) == 9 && sNums.get(9) == 9);
+    }
+
+    
+    
+    public static boolean check(int[][] sudoku) {
+        for(int i = 0; i < sudoku.length; i++){
+            for(int j = 0; j < sudoku.length; j++){
+                if(sudoku[i][j] == 0) return false;
+                //sor ellenőrzés
+                for(int r = 0; r < sudoku.length; r++){
+                    if(sudoku[i][r] == sudoku[i][j] && j != r){
+                        return false;
+                    }
+                }
+                //oszlop ellenőrzés
+                for(int c = 0; c < sudoku.length; c++){
+                    if(sudoku[c][j] == sudoku[i][j] && i != c){                    
+                        return false;
+                    } 
+                } 
+            }
+        }
+        return true;
     }
     
+    
+    
+    
+  public static int greedy(int[] dice){
+    int result = 0;
+    int forOne = 0, forSix = 0, forThree = 0, forFour = 0, forFive = 0, forTwo = 0;
+    for(int i = 0; i < dice.length; i++){
+        if(dice[i] == 1) forOne++;
+        if(dice[i] == 1 && forOne == 3){
+            result += 1000;
+            forOne = 0;
+        }
+        
+        if(dice[i] == 6) forSix++;
+        if(dice[i] == 6 && forSix == 3){
+            result += 600;
+            forSix = 0;
+        }
+        
+        if(dice[i] == 5) forFive++;
+        if(dice[i] == 5 && forFive == 3){
+            result += 500;
+            forFive = 0;
+        } 
+        
+        if(dice[i] == 4) forFour++; 
+        if(dice[i] == 4 && forFour == 3){
+            result += 400;          
+            forFour = 0;
+        } 
+        
+        if(dice[i] == 3) forThree++;
+        if(dice[i] == 3 && forThree == 3){
+            result += 300;
+            forThree = 0;
+        }  
+        
+        if(dice[i] == 2) forTwo++;
+        if(dice[i] == 2 && forTwo == 3){
+            result += 200;
+            forTwo = 0;
+        }  
+    }
+    if(forOne > 0) result += forOne * 100;
+    if(forFive> 0) result += forFive * 50;
+    
+    
+    
+    return result;
+  }    
+  
+  
+  public static int greedyRefactor(int[] Dice) {
+      int result = 0;
+      Map<Integer, Integer> nums = new HashMap<>();
+      for(int i = 0; i < Dice.length; i++){
+          if(nums.containsKey(Dice[i]))
+          nums.put(Dice[i], nums.get(Dice[i])+1);
+          else
+          nums.put(Dice[i], 1);
+      }    
+      System.out.println(nums);
+      if(nums.containsKey(1)) result += nums.get(1)%3 * 100;
+      if(nums.containsKey(1)) result += nums.get(1)/3 * 1000;
+      if(nums.containsKey(2)) result += nums.get(2)/3 * 200;
+      if(nums.containsKey(3)) result += nums.get(3)/3 * 300;
+      if(nums.containsKey(4)) result += nums.get(4)/3 * 400;
+      if(nums.containsKey(5)) result += nums.get(5)/3 * 500; 
+      if(nums.containsKey(5)) result += nums.get(5)%3 * 50;
+      if(nums.containsKey(6)) result += nums.get(6)/3 * 600;
+      
+      
+      return result;
+  }
+    
+    
+    
+    
+    
+    public static int parseInt_2(String numStr) {
+        if(numStr.equals("")) return -1;
+        String[] numbers = numStr.split("[ ]");
+        String tmpNum = "";
+        for(int i = 0; i < numbers.length; i++){
+            System.out.println(numbers[i] + " : " + i);
+            if ("thousand".equals(numbers[i]) && i == numbers.length-1) tmpNum += "000"; 
+            else if("thousand".equals(numbers[i]) && !numStr.contains("hundred")) tmpNum += "0";
+            
+            if(numbers[i].contains("-")){
+                
+            }
+            else
+            {
+                if("and".equals(numbers[i-1])) tmpNum += "0";
+                if("three".equals(numbers[i])) tmpNum += "3";
+            }
+        
+        }
+        int result = Integer.parseInt(tmpNum);
+        return result;
+    }
+    
+    public static int parseInt(String numStr) {
+        if(numStr.equals("")) return -1;
+        String[] numbers = numStr.split("[ ]");
+        String tmpNum = "";
+        for(int i = 0; i < numbers.length; i++){
+            //if("thousand".equals(numbers[i]) || "hundred".equals(numbers[i]) || "and".equals(numbers[i]) ) continue;
+            if(!numbers[i].contains("teen")) {
+                if("eleven".equals(numbers[i])) tmpNum += "11";
+                else if("twelve".equals(numbers[i])) tmpNum += "12";               
+                else if("one".equals(numbers[i])) tmpNum += "1";
+                else if(numbers[i].startsWith("tw")) tmpNum += "2";     
+                else if(numbers[i].startsWith("th") && !"thousand".equals(numbers[i])) tmpNum += "3";
+                else if(numbers[i].startsWith("fo")) tmpNum += "4";
+                else if(numbers[i].startsWith("fi")) tmpNum += "5";
+                else if(numbers[i].startsWith("six")) tmpNum += "6";
+                else if(numbers[i].startsWith("seven")) tmpNum += "7";
+                else if(numbers[i].startsWith("eight")) tmpNum += "8";
+                else if(numbers[i].startsWith("nin")) tmpNum += "9";
+                if(numbers[i].contains("-")){
+                    String tmp = numbers[i].substring(numbers[i].indexOf("-")+1, numbers[i].length());
+                    if("one".equals(tmp)) tmpNum += "1";
+                    if("two".equals(tmp)) tmpNum += "2";
+                    if("three".equals(tmp)) tmpNum += "3";
+                    if("four".equals(tmp)) tmpNum += "4";
+                    if("five".equals(tmp)) tmpNum += "5";
+                    if("six".equals(tmp)) tmpNum += "6";
+                    if("seven".equals(tmp)) tmpNum += "7";
+                    if("eight".equals(tmp)) tmpNum += "8";
+                    if("nine".equals(tmp)) tmpNum += "9";
+                }
+                else if(numbers[i].endsWith("y")) tmpNum += "0";
+                if("thousand".equals(numbers[i]) && !numStr.contains("hundred")) tmpNum += "0";
+                //if("hundred".equals(numbers[i]) && !numStr.contains("y")) tmpNum += "0";
+            }
+            else {
+                if("thirteen".equals(numbers[i])) tmpNum += "13";
+                if("fourteen".equals(numbers[i])) tmpNum += "14";
+                if("fifteen".equals(numbers[i])) tmpNum += "15";
+                if("sixteen".equals(numbers[i])) tmpNum += "16";
+                if("seventeen".equals(numbers[i])) tmpNum += "17";
+                if("eighteen".equals(numbers[i])) tmpNum += "18";
+                if("nineteen".equals(numbers[i])) tmpNum += "19";    
+            }
+        }
+        int result = Integer.parseInt(tmpNum);
+        return result;
+    }
+    
+    public static int[] snail(int[][] array) {
+        int[] result = new int[array.length * array.length];
+        int index = 0; //új tömb indexe
+        int m = array.length;//sor
+        int n = m;//oszlop
+        int i, k = 0, l = 0; 
+        while (k < m && l < n){
+            for(i = l; i < n; ++i) {
+                result[index] = array[k][i];
+                index++;
+            }
+            k++;//Következő körre ez lesz az első sor.
+            for(i = k; i < m; ++i) {//K-tól kezdjük, mert az most pont az a sor, ami kell
+                result[index] = array[i][n-1];//Az n az fix, mert csak az utolsó sort szeretnénk megszerezni
+                index++;
+            }
+            n--;//A következő körben egyel korábban lesz az utolsó oszlop.
+            if(k < m) {
+                for(i = n-1; i >= l; --i) {
+                  result[index] = array[m-1][i];
+                  index++;
+                }
+                m--;
+            }
+            if(l < n) {
+                for(i = m-1; i >= k; --i) {
+                  result[index] = array[i][l];
+                  index++;
+                }
+                l++;
+            }
+        }
+        return result;
+   } 
     public static String delAt(String str, int n){
         return str.replace(String.valueOf(str.charAt(n)), "");
     }
     
     public static int getCount(String str) {
-    int vowelsCount = 0;   
-    if(str.contains("[aeiou]")) {
-        vowelsCount++;
-    }
+        int vowelsCount = 0;   
+        if(str.contains("[aeiou]")) {
+            vowelsCount++;
+        }
 
-    return vowelsCount;
-  }
+        return vowelsCount;
+    }
     
+     public static String firstEnd(String str){
+        String mid = str.substring(1, str.length()-1);
+        return str.charAt(str.length()-1) + mid + str.charAt(0);
+    }   
 
     public static String giveBack(String str){
         return str.charAt(0) + str + str.charAt(str.length()-1);
